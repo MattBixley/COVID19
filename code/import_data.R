c59c4b9f528877d58a47d640420d809dc2e12f10
Matt Bixley <mattbixley72@gmail.com>
2020-03-26 10:26:09 +1300

    added some population data

---
+++ code/import_data.R
@@ -0,0 +1,45 @@
+# Johns Hiopkins Dataset
+
+jhu_confirmed <- paste("https://raw.githubusercontent.com/CSSEGISandData/", 
+                       "COVID-19/master/csse_covid_19_data/", "csse_covid_19_time_series/", 
+                       "time_series_covid19_confirmed_global.csv", sep = "")
+
+jhu_deaths <- paste("https://raw.githubusercontent.com/CSSEGISandData/", 
+                    "COVID-19/master/csse_covid_19_data/", "csse_covid_19_time_series/", 
+                    "time_series_covid19_deaths_global.csv", sep = "")
+
+# Download
+download.file(url = jhu_confirmed, destfile = "data/jhu_confirmed.csv")
+download.file(url = jhu_deaths, destfile = "data/jhu_deaths.csv")
+
+cases <- read_csv(jhu_confirmed) %>% rename(state = "Province/State", country = "Country/Region") %>% 
+  pivot_longer(-c(state, country, Lat, Long), names_to = "Date", values_to = "cumulative_cases") %>% 
+  mutate(Date = mdy(Date)) %>%
+  
+  # cumultaive cases by province and country
+  group_by(country,province) %>% 
+  mutate(cases = cumulative_cases - lag(cumulative_cases)) %>% 
+  group_by(country, Date) %>%
+  mutate(country_cum_cases = sum(cumulative_cases)) %>% 
+  ungroup() %>% 
+  mutate(country_cases = country_cum_cases - lag(country_cum_cases)) %>% 
+  
+  # clean up the NA and negative values
+  mutate(country_cases = ifelse(country_cases < 0, 0, country_cases)) %>% 
+  mutate(cases = ifelse(is.na(cases), 0, cases ))
+
+deaths <- read_csv(jhu_deaths) %>% rename(state = "Province/State", country = "Country/Region") %>% 
+  pivot_longer(-c(state.area, country, Lat, Long), names_to = "Date", values_to = "cumulative_deaths") %>% 
+  mutate(Date = mdy(Date)) %>%
+  
+  # cumultaive deaths by province and country
+  group_by(country,province) %>% 
+  mutate(deaths = cumulative_deaths - lag(cumulative_deaths)) %>% 
+  group_by(country, Date) %>%
+  mutate(country_cum_deaths = sum(cumulative_deaths)) %>% 
+  ungroup() %>% 
+  mutate(country_deaths = country_cum_deaths - lag(country_cum_deaths)) %>% 
+  
+  # clean up the NA and negative values
+  mutate(country_deaths = ifelse(country_deaths < 0, 0, country_deaths)) %>% 
+  mutate(deaths = ifelse(is.na(deaths), 0, deaths ))
---
+++ code/population.R
@@ -0,0 +1,29 @@
+# read population data from wiki
+# https://www.worldometers.info/world-population/population-by-country/
+
+library(rvest)
+library(xml2)
+
+world_url <- "https://www.worldometers.info/world-population/population-by-country/"
+
+world_pop <- read_html(pop_url) %>%
+  html_nodes("table") %>%
+  html_table(fill=TRUE) %>%
+  .[[1]] %>% 
+  rename(country = "Country (or dependency)", population = "Population (2020)", 
+         density = "Density (P/Km²)", area = "Land Area (Km²)") %>% 
+  select(country, population, density, area) %>% 
+  map_df(str_replace_all, pattern = ",", replacement = "") %>% 
+  map_df(str_remove, pattern = "%") %>% 
+  mutate_at(vars(-country), as.numeric)
+    
+# population of states in china, australia and usa
+
+usa <- "https://simple.wikipedia.org/wiki/List_of_U.S._states_by_population"
+china <- "https://en.wikipedia.org/wiki/List_of_Chinese_administrative_divisions_by_population"
+
+# Australia - ""
+australia <- as.data.frame(state = c("New South Wales", "Queensland", "Victoria", "Australian Capital Territory",
+                                     "Northern Territory","South Australia","Tasmania","Western Australia"),
+                           population = c(8089525, 5095100, 6594804, 426709, 245869, 1751693, 534281, 2621680),
+                           area = c(809952,1851736,237657, 2358, 1419630, 1044353, 90758, 2642753))
