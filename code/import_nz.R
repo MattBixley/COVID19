22a8862e4b95c4d7c2e2389c463c32ea7f8f3d9f
Matt Bixley <mattbixley72@gmail.com>
2020-04-03 15:03:01 +1300

    still can't idiot proof the MOH data

--- code/import_nz.R
+++ code/import_nz.R
@@ -1,24 +1,24 @@
 ### function to insert todays values for NZ
 
-insert_today <- function(nzcases = 0, nzdeaths = 0){
-  insert <- covid19 %>% filter(country == "New_Zealand", date == max(date)) %>% 
-
-    # add date values
-    mutate(date = date + 1) %>% 
-    mutate(day = day(date), month = month(date), year = year(date)) %>% 
-    
-    # add the cases
-    mutate(cases = nzcases, deaths = nzdeaths) %>% 
-    mutate(cum_cases = cum_cases + nzcases, cum_deaths = cum_deaths + nzdeaths) %>% 
-    mutate(per_million = round(cum_cases/population*1000000,1)) %>%
-    mutate(cases_million = round(cases/population*1000000,1))
-  
-
-  
-  newcovid <- rbind(covid19, insert) %>% 
-  arrange(date, country)
-  return(newcovid)
-}
+# insert_today <- function(nzcases = 0, nzdeaths = 0){
+#   insert <- covid19 %>% filter(country == "New_Zealand", date == max(date)) %>% 
+# 
+#     # add date values
+#     mutate(date = date + 1) %>% 
+#     mutate(day = day(date), month = month(date), year = year(date)) %>% 
+#     
+#     # add the cases
+#     mutate(cases = nzcases, deaths = nzdeaths) %>% 
+#     mutate(cum_cases = cum_cases + nzcases, cum_deaths = cum_deaths + nzdeaths) %>% 
+#     mutate(per_million = round(cum_cases/population*1000000,1)) %>%
+#     mutate(cases_million = round(cases/population*1000000,1))
+#   
+# 
+#   
+#   newcovid <- rbind(covid19, insert) %>% 
+#   arrange(date, country)
+#   return(newcovid)
+# }
 
 #covid19 <- insert_today(cases = 85, deaths = 0) %>% filter(country == "New_Zealand")
 
@@ -46,9 +46,15 @@ nz_moh <- bind_rows(nz_moh_cases, nz_moh_probable) %>%
   rename(date = "Date of report", last_country = "Last country before return", flight = "Flight number",
          flight_date = "Arrival date", age_group = "Age group", travel = "International travel", 
          flight_dep_date = "Flight departure date") %>% 
-  mutate(date = dmy(date), flight_dep_date = dmy(flight_dep_date), flight_date = dmy(flight_date)) %>% 
-  mutate(date = ifelse(date == max(date),  max(date) - 1 , date )) %>%
-  mutate(date = as_date(date)) %>% 
+  
+  # deal with flights and merge to one date, last known date
+  mutate(flight_date = ifelse(is.na(flight_date), flight_dep_date, flight_date)) %>% 
+  mutate(date = dmy(date), flight_date = dmy(flight_date)) %>%
+  select(-flight_dep_date) %>% 
+  
+  # hack to push the ovelap cases back one day
+  #mutate(date = ifelse(date == max(date),  max(date) - 1 , date )) %>%
+  #mutate(date = as_date(date)) %>% 
   arrange(date)
 
 write_csv(nz_moh, "data/nz_moh.csv")
@@ -71,8 +77,8 @@ nzcdc <- tibble(date = seq(min(covid19$date), max(covid19$date), 1),
   mutate_at(vars(cases), funs(replace_na(., 0))) %>% 
   mutate(per_million = round(cum_cases/population*1000000,3)) %>%  # cumulative per million
   mutate(cases_million = round(cases/population*1000000,3)) %>% # daily cases per million
-  mutate(deaths = 0, cum_deaths = 0) %>% 
-  filter(date != max(date))
+  mutate(deaths = 0, cum_deaths = 0) #%>% 
+  #filter(date != max(date))
   
 covid19 <- covid19 %>% filter(country != "New_Zealand") %>% 
   bind_rows(.,nzcdc)
